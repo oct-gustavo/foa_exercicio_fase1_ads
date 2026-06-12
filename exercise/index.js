3bca316eabd46a7d0eeb195e3e0a19a81344d82a
oct-gustavo <dearaujoluisgustavo95@gmail.com>
2026-06-11 22:15:22 -0300

    To-Do List (v1.1) status: Done

--- exercise/index.js
+++ exercise/index.js
@@ -9,6 +9,24 @@ function addTask(){
         let li = document.createElement("li")
         li.innerHTML = inputBox.value;
         taskList.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
 }
+
+taskList.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+inputBox.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        addTask();
+    }
+});
